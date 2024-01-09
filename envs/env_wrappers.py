c06c3717f65ffc480098ff3129c83f30e6ae428a
Luo Ming <ming.lok@outlook.com>
2024-01-09 16:28:39 +0800

    add class CloudpickleWrapper for communication between processes

--- envs/env_wrappers.py
+++ envs/env_wrappers.py
@@ -10,6 +10,23 @@ import numpy as np
 from abc import ABC, abstractmethod
 
 
+class CloudpickleWrapper(object):
+    """
+    Uses cloudpickle to serialize contents (otherwise multiprocessing tries to use pickle)
+    """
+
+    def __init__(self, x):
+        self.x = x
+
+    def __getstate__(self):
+        import cloudpickle
+        return cloudpickle.dumps(self.x)
+
+    def __setstate__(self, ob):
+        import pickle
+        self.x = pickle.loads(ob)
+
+
 class ShareVecEnv(ABC):
     """
     An abstract asynchronous, vectorized environment.
