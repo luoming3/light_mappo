84e0583031302b5da389cdf0ee0467dcbf605850
Luo Ming <ming.lok@outlook.com>
2024-01-23 15:35:13 +0800

    fix tiny, fix scripts/train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-env="simple_car"
-guide_point=100
-scenario="guide_point_"${guide_point}
+env="box2d_car"
+scenario="share"
 num_landmarks=3
 num_agents=4
 algo="mappo"  # default="mappo", choices=["rmappo", "mappo"]
@@ -14,4 +13,4 @@ echo "env is ${env}"
 
 CUDA_VISIBLE_DEVICES=0 python ${current_dir}/../train/train.py --env_name ${env} --algorithm_name ${algo} \
 --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} \
---n_training_threads 1 --n_render_rollout_threads 32 --episode_length 1024
+--n_training_threads 1 --n_rollout_threads 32 --episode_length 1024
