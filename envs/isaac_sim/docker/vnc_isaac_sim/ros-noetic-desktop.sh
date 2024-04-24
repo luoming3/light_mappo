ca56867039cbc790c67bbc7389b73f41d2865698
Luo Ming <ming.luo@lscm.hk>
2024-04-23 20:27:50 -0700

    add isaac-sim dockerfile

---
+++ envs/isaac_sim/docker/vnc_isaac_sim/ros-noetic-desktop.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -eu
+
+[[ "$(lsb_release -sc)" == "focal" ]] || exit 1
+ROS_DISTRO=noetic
+
+## Setup your sources.list
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+## Set up your keys
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
+
+## Installation
+apt update
+apt-get install -y ros-${ROS_DISTRO}-desktop
+
+## Environment setup
+echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
+
+## Dependencies for building packages
+apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+
+## Initialize rosdep
+rosdep init 
+rosdep update
+
+grep -F "ROS_IP" ~/.bashrc || echo "export ROS_IP=127.0.0.1" >> ~/.bashrc
+
+grep -F "ROS_MASTER_URI" ~/.bashrc || echo "export ROS_MASTER_URI=http://\$ROS_IP:11311" >> ~/.bashrc
+
+echo ""
+echo "Success installing ROS ${ROS_DISTRO}"
+echo "Run 'source ~/.bashrc'"
+echo ""
+echo "If any error occurs, please refer to the following URL."
+echo "https://github.com/Tiryoh/ros_setup_scripts_ubuntu/"
+echo ""
---
+++ envs/isaac_sim/docker/vnc_isaac_sim/start-vnc.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# start vnc server
+vncserver -kill :*
+vncserver :77 -localhost no
+
+# start novnc server
+ps -ef | grep websockify | awk '{print $2}' | xargs kill -9 || echo ""
+websockify --web /usr/share/novnc 80 localhost:5977
