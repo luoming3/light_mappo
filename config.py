96b705cd67095eacd09a3fbcd1a6894d53fe7b81
Luo Ming <ming.luo@lscm.hk>
2024-06-06 14:50:15 +0800

    update config, use gpu to train by default

--- config.py
+++ config.py
@@ -169,9 +169,9 @@ def get_config():
     parser.add_argument("--seed", type=int, default=1, help="Random seed for numpy/torch")
     parser.add_argument(
         "--cuda",
-        action="store_true",
-        default=False,
-        help="by default False, will use CPU to train; or else will use GPU;",
+        action="store_false",
+        default=True,
+        help="by default True, will use GPU to train; or else will use CPU;",
     )
     parser.add_argument(
         "--cuda_deterministic",
--- scripts/train.sh
+++ scripts/train.sh
@@ -13,4 +13,4 @@ echo "env is ${env}"
 
 CUDA_VISIBLE_DEVICES=0 python -u ${current_dir}/../train/train.py --env_name ${env} --algorithm_name ${algo} \
 --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} \
---cuda --n_training_threads 4 --n_rollout_threads 32 --episode_length 1024 --layer_N 1 --hidden_size 64 --num_env_steps 300000000
+--n_training_threads 4 --n_rollout_threads 32 --episode_length 1024 --layer_N 1 --hidden_size 64 --num_env_steps 300000000
