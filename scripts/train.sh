732903c9e05731ba7addb5e781bbbf4486dc181e
Luo Ming <ming.luo@lscm.hk>
2024-06-06 14:32:41 +0800

    update scripts/train.sh, update training params for isaac sim

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-env="box2d_car"
+env="isaac-sim"
 scenario="share"
 num_landmarks=3
 num_agents=4
@@ -13,4 +13,4 @@ echo "env is ${env}"
 
 CUDA_VISIBLE_DEVICES=0 python -u ${current_dir}/../train/train.py --env_name ${env} --algorithm_name ${algo} \
 --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} \
---n_training_threads 4 --n_rollout_threads 32 --episode_length 2048 --layer_N 2 --cuda --hidden_size 128 --num_env_steps 100000000
+--cuda --n_training_threads 4 --n_rollout_threads 32 --episode_length 1024 --layer_N 1 --hidden_size 64 --num_env_steps 300000000
