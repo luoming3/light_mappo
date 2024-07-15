61ecd5147a73c0c054e1ea838faca3c159f4e2df
Luo Ming <ming.lok@outlook.com>
2024-07-15 08:13:56 +0000

    update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -11,6 +11,11 @@ current_dir=$(cd $(dirname $0); pwd)
 
 echo "env is ${env}"
 
+cd ${current_dir}/.. && \
+    git branch && \
+    git log -1 && \
+    git diff
+
 CUDA_VISIBLE_DEVICES=0 python -u ${current_dir}/../train/train.py --env_name ${env} --algorithm_name ${algo} \
 --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} \
---n_training_threads 4 --n_rollout_threads 32 --episode_length 1024 --layer_N 1 --hidden_size 64 --num_env_steps 300000000
+--n_training_threads 4 --n_rollout_threads 256 --episode_length 1024 --layer_N 3 --hidden_size 128 --num_env_steps 500000000
