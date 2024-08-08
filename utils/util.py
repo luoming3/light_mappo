b094c1cbc981898dccfff5fd3bf2d1929876f98d
Luo Ming <ming.luo@lscm.hk>
2024-08-08 16:49:50 +0800

    add funs to translate orientation between euler and quaternion

--- utils/util.py
+++ utils/util.py
@@ -88,6 +88,41 @@ def _t2n(x):
     return x.detach().cpu().numpy()
 
 
+def quaternion_to_euler(quaternion: torch.Tensor) -> torch.Tensor:
+
+    w, x, y, z = torch.unbind(quaternion, dim=quaternion.dim() - 1)
+
+    euler_angles: torch.Tensor = torch.stack(
+        (
+            torch.atan2(2.0 * (w * x + y * z), 1.0 - 2.0 * (x * x + y * y)),
+            torch.asin(2.0 * (w * y - z * x)),
+            torch.atan2(2.0 * (w * z + x * y), 1.0 - 2.0 * (y * y + z * z)),
+        ),
+        dim=-1,
+    )
+
+    return euler_angles
+
+
+def euler_to_quaternion(euler: torch.Tensor) -> torch.Tensor:
+    euler = torch.as_tensor(euler)
+    r, p, y = torch.unbind(euler, dim=-1)
+    cy = torch.cos(y * 0.5)
+    sy = torch.sin(y * 0.5)
+    cp = torch.cos(p * 0.5)
+    sp = torch.sin(p * 0.5)
+    cr = torch.cos(r * 0.5)
+    sr = torch.sin(r * 0.5)
+
+    qw = cr * cp * cy + sr * sp * sy
+    qx = sr * cp * cy - cr * sp * sy
+    qy = cr * sp * cy + sr * cp * sy
+    qz = cr * cp * sy - sr * sp * cy
+
+    quaternion = torch.stack([qw, qx, qy, qz], dim=-1)
+
+    return quaternion
+
 @timethis
 def main():
     time.sleep(1)
