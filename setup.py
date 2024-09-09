793a756109f16191129991183cb62d7cbc223960
Luo Ming <ming.luo@lscm.hk>
2024-09-09 16:08:01 +0800

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+# 读取 requirements.txt 文件
+with open('requirements_3_10.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='light_mappo',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=requirements,
+)
