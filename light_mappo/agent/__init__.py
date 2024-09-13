8d00677c2b9d2051f54f5959169208c8fe8abb6f
Luo Ming <ming.luo@lscm.hk>
2024-09-13 17:19:09 +0800

    fix tiny, fix mappo agent to load model

--- light_mappo/agent/__init__.py
+++ light_mappo/agent/__init__.py
@@ -9,7 +9,6 @@ from light_mappo.utils.util import _t2n
 from light_mappo.envs.isaac_sim.env_core import OBS_DIM, ACTION_SPACE
 
 MODEL_DIR = "/app/deploy/models/actor.pt"
-# MODEL_DIR = "/home/user/Desktop/git/light_mappo/scripts/results/MyEnv/MyEnv/mappo/check/run5/models/actor.pt"
 
 
 class Agent(object):
@@ -36,7 +35,7 @@ class Agent(object):
         )
         self.args = all_args
         self.num_agent = 1
-        self.num_evn = 1
+        self.num_env = 1
 
         if os.path.isfile(MODEL_DIR):
             self.restore(MODEL_DIR)
@@ -45,7 +44,8 @@ class Agent(object):
 
     def restore(self, model_dir=None):
         """Restore policy's networks from a saved model."""
-        policy_actor_state_dict = torch.load(model_dir)
+        policy_actor_state_dict = torch.load(model_dir,
+                                             map_location=torch.device("cpu"))
         self.policy.actor.load_state_dict(policy_actor_state_dict)
 
     def prep_rollout(self):
@@ -54,14 +54,14 @@ class Agent(object):
     def act(self, obs):
         rnn_states = np.zeros(
             (
-                self.num_evn,
+                self.num_env,
                 self.num_agent,
                 self.args.recurrent_N,
                 self.args.hidden_size,
             ),
             dtype=np.float32,
         )
-        masks = np.ones((self.num_evn, self.num_agent, 1), dtype=np.float32)
+        masks = np.ones((self.num_env, self.num_agent, 1), dtype=np.float32)
 
         actions, _ = self.policy.act(
             obs,
