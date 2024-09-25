44f4e91e59884606251d406907baeddb9d134843
Luo Ming <ming.luo@lscm.hk>
2024-09-25 03:00:40 +0000

    update mappo_node for navigation (single car)

--- ros_ws/src/maxbot_sim/scripts/mappo_node.py
+++ ros_ws/src/maxbot_sim/scripts/mappo_node.py
@@ -6,23 +6,34 @@ from geometry_msgs.msg import PoseWithCovarianceStamped
 from nav_msgs.msg import Odometry
 import numpy as np
 import math
+import copy
 
 from light_mappo.agent import Agent
 from make_plan import get_path
 
 # /cmd_vel topic
 ACION_PUBLISHER = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+STATUS_RUNNING = 0
+STATUS_SUCCESS = 1
+STATUS_FAILURE = 2
+STATUS_UNKNOWN = 3
 
 
 class MappoNode:
 
-    def __init__(self) -> None:
-        self.position = None
-        self.orientation = None
-        self.euler_ori = None
-        self.velocities = None
+    def __init__(self, start, goal) -> None:
+        self.position = np.array([])
+        self.orientation = np.array([])
+        self.euler_ori = np.array([])
+        self.velocities = np.array([])
         self.force = np.array([0., 0.])
         self.path = []
+        self.start = np.array(start)
+        self.goal = np.array(goal)
+        self.path = get_path(start, goal)
+        if len(self.path) == 0:
+            raise RuntimeError("can't find a path")
+        self.guide_point = self.path[0]
 
         self.amcl_subscriber = rospy.Subscriber("/amcl_pose",
                                                 PoseWithCovarianceStamped,
@@ -57,10 +68,10 @@ class MappoNode:
 
     def get_obs(self):
         ## TODO: calculate the rpos of car and dest
-        if (not np.any(self.position)) or (not np.any(
-                self.velocities)) or (not np.any(self.orientation)):
-            return None
-        rpos_car_dest_norm = normalized(np.array([0, 0]) - self.position)
+        if (self.position.size == 0) or (self.velocities.size== 0) or \
+            (self.orientation.size == 0):
+            return np.array([])
+        rpos_car_dest_norm = normalized(self.guide_point - self.position)
         maxbot_linear_velocities = self.velocities
         maxbot_orientation = np.array([self.euler_ori[2]])
         force = self.force
@@ -74,12 +85,44 @@ class MappoNode:
         # TODO
         pass
 
-    def get_path(self, start, goal):
-        self.path = get_path(start, goal)
+    def clip_path(self):
+        path = copy.deepcopy(self.path)
+        if len(path) > 1:
+            first_point = path[0]
+            second_point = path[1]
+            angle = np.dot(second_point - first_point,
+                           self.position - first_point)
 
-    def get_guide_point(self):
-        # TODO
-        pass
+            if angle < 0:
+                pass
+            else:
+                self.path = self.path[1:]
+            self.guide_point = self.path[0]
+
+    def step(self):
+        done = False
+        status = STATUS_RUNNING
+
+        obs = self.get_obs()
+        if obs.size == 0:
+            rospy.logwarn("observation is None")
+            return done, status
+        # arrival
+        current_dist_to_goal = np.linalg.norm(self.position - self.goal)
+        current_dist_to_point = np.linalg.norm(self.position -
+                                               self.guide_point)
+        if current_dist_to_goal < 0.5:
+            done = True
+            status = STATUS_SUCCESS
+        if current_dist_to_point > 5:
+            done = False
+            status = STATUS_FAILURE
+
+        self.clip_path()
+        action = get_action(obs)
+        publish_action(action)
+
+        return done, status
 
 
 def get_action(obs):
@@ -92,8 +135,8 @@ def get_action(obs):
     return agent.act(obs)
 
 
-def public_action(action):
-    if not np.any(action):
+def publish_action(action):
+    if action.size == 0:
         return
     twist = Twist()
     twist.linear.x = action[0]
@@ -133,20 +176,31 @@ def normalized(v, axis=0):
     return result
 
 
-def main():
+def main(start, goal):
     rospy.init_node("mappo_node")
-    mappo_node = MappoNode()
+
+    mappo_node = MappoNode(start, goal)
 
     # pub FPS: 10 Hz
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
-        obs = mappo_node.get_obs()
-        action = get_action(obs)
-        public_action(action)
-        rate.sleep()
+        done, status = mappo_node.step()
+        if not done:
+            rate.sleep()
+            continue
+
+        if status == STATUS_SUCCESS:
+            rospy.loginfo("task success")
+            publish_action(np.array([0, 0]))
+            return
+        if status == STATUS_FAILURE:
+            rospy.logerr("task failure")
+            return
 
     rospy.spin()
 
 
 if __name__ == "__main__":
-    main()
+    start = (-5, 3)
+    goal = (5, -3)
+    main(start, goal)
