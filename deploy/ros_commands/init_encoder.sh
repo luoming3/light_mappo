8e01732155f95ee275af6f5540da23bcb033a067
Luo Ming <ming.luo@lscm.hk>
2024-11-18 17:53:06 +0800

    update tiny

--- deploy/ros_commands/init_encoder.sh
+++ deploy/ros_commands/init_encoder.sh
@@ -8,5 +8,5 @@ SCRIPTS_PATH="/app/ros_ws/src/maxbot_real/scripts"
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/app/ros_ws/devel/setup.bash" || echo ""
 
-# init amcl
+# init encoder
 python3 -u ${SCRIPTS_PATH}/init_encoder.py
