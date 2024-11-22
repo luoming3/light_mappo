449d175f70957471562ceaef2c54bf7e12bf65ab
Luo Ming <ming.luo@lscm.hk>
2024-11-22 07:51:56 +0000

    improve get_car_position()

--- ros_ws/src/maxbot_real/scripts/mappo_node.py
+++ ros_ws/src/maxbot_real/scripts/mappo_node.py
@@ -31,10 +31,11 @@ STATUS_UNKNOWN = 3
 
 class MappoNode:
 
-    def __init__(self, start, goal, id, gamma_, l_) -> None:
+    def __init__(self, start, goal, id, gamma_, w, l) -> None:
         self.id = id
         self.gamma_ = gamma_
-        self.l_ = l_
+        self.w = w
+        self.l = l
         self.position = np.array([])
         self.orientation = np.array([])
         self.euler_ori = np.array([])
@@ -106,23 +107,29 @@ class MappoNode:
         x = self.position[0]
         y = self.position[1]
         if self.id == 1:
-            x0 = x - math.cos(phi) * self.l_
-            y0 = y + math.sin(phi) * self.l_
+            l_ = math.sqrt(self.w**2 + self.l**2) / 2
+            x0 = x - math.cos(phi) * l_
+            y0 = y + math.sin(phi) * l_
         elif self.id == 2:
-            x0 = x + math.sin(phi) * self.l_
-            y0 = y + math.cos(phi) * self.l_
+            l_ = math.sqrt(self.w**2 + self.l**2) / 2
+            x0 = x + math.sin(phi) * l_
+            y0 = y + math.cos(phi) * l_
         elif self.id == 3:
-            x0 = x - math.sin(phi) * self.l_
-            y0 = y - math.cos(phi) * self.l_
+            l_ = self.w / 2
+            x0 = x - math.sin(phi) * l_
+            y0 = y - math.cos(phi) * l_
         elif self.id == 4:
-            x0 = x + math.sin(phi) * self.l_
-            y0 = y + math.cos(phi) * self.l_
+            l_ = self.w / 2
+            x0 = x + math.sin(phi) * l_
+            y0 = y + math.cos(phi) * l_
         elif self.id == 5:
-            x0 = x - math.sin(phi) * self.l_
-            y0 = y - math.cos(phi) * self.l_
+            l_ = math.sqrt(self.w**2 + self.l**2) / 2
+            x0 = x - math.sin(phi) * l_
+            y0 = y - math.cos(phi) * l_
         elif self.id == 6:
-            x0 = x + math.cos(phi) * self.l_
-            y0 = y - math.sin(phi) * self.l_
+            l_ = math.sqrt(self.w**2 + self.l**2) / 2
+            x0 = x + math.cos(phi) * l_
+            y0 = y - math.sin(phi) * l_
         else:
             raise RuntimeError("unknown id")
         self.car_position = np.array([x0, y0])
@@ -220,8 +227,8 @@ def normalized(v, axis=0):
 
 def main(*args):
     rospy.init_node("mappo_node")
-    start, goal, id, gamma_, l_ = args
-    mappo_node = MappoNode(start, goal, id, gamma_, l_)
+    start, goal, id, gamma_, w, l = args
+    mappo_node = MappoNode(start, goal, id, gamma_, w, l)
 
     # pub FPS: 10 Hz
     rate = rospy.Rate(10)
@@ -251,11 +258,12 @@ if __name__ == "__main__":
         goal = ast.literal_eval(args[1])
         id = int(args[2])
         gamma_ = float(args[3])
-        l_ = float(args[4])
+        w = float(args[4])
+        l = float(args[5])
     except:
         raise RuntimeError("input args is invalid")
     else:
-        main(start, goal, id, gamma_, l_)
+        main(start, goal, id, gamma_, w, l)
     finally:
         # stop maxbot
         os.system("rostopic pub -1 /cmd_vel geometry_msgs/Twist \
