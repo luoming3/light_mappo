39755a3fb4d4e271b4a616e8dcd03e3ff3cc05db
Luo Ming <ming.luo@lscm.hk>
2024-12-16 07:13:02 +0000

    don't init encoder when execute init.sh

--- deploy/ros_commands/init.sh
+++ deploy/ros_commands/init.sh
@@ -9,7 +9,7 @@ source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/app/ros_ws/devel/setup.bash" || echo ""
 
 # init rotary encoder
-python3 -u ${SCRIPTS_PATH}/init_encoder.py
+# python3 -u ${SCRIPTS_PATH}/init_encoder.py
 
 # init pose
 python3 -u ${SCRIPTS_PATH}/set_init_pose.py "${1}" "${2}" "${3}" "${4}"
