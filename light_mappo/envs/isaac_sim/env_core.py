ede0cbc025626d8adefa368674ded51b2fa4a5bb
Wenze Li <wenze.li@lscm.hk>
2024-12-17 17:44:25 +0800

    Update env_core.py

--- light_mappo/envs/isaac_sim/env_core.py
+++ light_mappo/envs/isaac_sim/env_core.py
@@ -69,6 +69,20 @@ class EnvCore(object):
             # from light_mappo.envs.isaac_sim.utils.scene import _randomizer
             # self.dr_randomizer = _randomizer
 
+    def post_pro_obs(self, init_obs):
+        rpos_car_dest_extracted = init_obs[:, :, :2]  
+        rpos_car_dest_norm = normalized(rpos_car_dest_extracted, dim=2)
+        joint_forces = init_obs[:, :, 5:]
+        joint_forces = torch.tanh(joint_forces/50)
+        env_obs_other_part =  init_obs[:, :, 2:5] 
+
+        observations = torch.cat(
+            (rpos_car_dest_norm, env_obs_other_part, joint_forces),
+            dim=2
+        )
+
+        return observations
+
     def reset(self, indices=[]):
         if len(indices) == 0:
             indices = self.env_indices
@@ -84,17 +98,7 @@ class EnvCore(object):
 
         # observations, shape is (env_num, agent_num, obs_dim)
         observations = self.get_observations()
-        rpos_car_dest_extracted = observations[:, :, :2]  # 切片提取 rpos_car_dest 的部分
-        rpos_car_dest_norm = normalized(rpos_car_dest_extracted, dim=2)
-        joint_forces = observations[:, :, 5:]
-        joint_forces = torch.tanh(joint_forces/50)
-        env_obs_other_part =  observations[:, :, 2:5] 
-
-        observations = torch.cat(
-            (rpos_car_dest_norm, env_obs_other_part, joint_forces),
-            dim=2
-        )
-
+        observations = self.post_pro_obs(observations)
         self.steps[indices] = 0
         
         if self.all_args.use_randomize:
@@ -113,16 +117,7 @@ class EnvCore(object):
 
         # observations, shape is (env_num, agent_num, obs_dim)
         observations = self.get_observations()
-        rpos_car_dest_extracted = observations[:, :, :2]  # 切片提取 rpos_car_dest 的部分
-        rpos_car_dest_norm = normalized(rpos_car_dest_extracted)
-        joint_forces = observations[:, :, 5:]
-        joint_forces = torch.tanh(joint_forces/50)
-        env_obs_other_part =  observations[:, :, 2:5] 
-
-        observations = torch.cat(
-            (rpos_car_dest_norm, env_obs_other_part, joint_forces),
-            dim=2
-        )
+        observations = self.post_pro_obs(observations)
         
         self.steps[indices] = 0
 
@@ -170,16 +165,7 @@ class EnvCore(object):
             )
         # print('observation randomize:', env_obs)
 
-        rpos_car_dest_extracted = env_obs[:, :, :2]  # 切片提取 rpos_car_dest 的部分
-        rpos_car_dest_norm = normalized(rpos_car_dest_extracted, dim=2)
-        joint_forces = env_obs[:, :, 5:]
-        joint_forces = torch.tanh(joint_forces/50)
-        env_obs_other_part =  env_obs[:, :, 2:5] 
-
-        env_obs = torch.cat(
-            (rpos_car_dest_norm, env_obs_other_part, joint_forces),
-            dim=2
-        )
+        env_obs = self.post_pro_obs(env_obs)
 
         current_car_position = self.get_world_poses()[0][:, 0:2]
         current_car_position.sub_(self.init_envs_positions[:, 0:2])
