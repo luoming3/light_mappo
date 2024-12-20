5265a41a8854927ec3c7a5f3287d9a6a4f991687
Luo Ming <ming.luo@lscm.hk>
2024-12-20 17:20:33 +0800

    improve init.sh command

--- deploy/ros_commands/init.sh
+++ deploy/ros_commands/init.sh
@@ -8,8 +8,11 @@ SCRIPTS_PATH="/app/ros_ws/src/maxbot_real/scripts"
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/app/ros_ws/devel/setup.bash" || echo ""
 
-# init rotary encoder
-# python3 -u ${SCRIPTS_PATH}/init_encoder.py
-
 # init pose
 python3 -u ${SCRIPTS_PATH}/set_init_pose.py "${1}" "${2}" "${3}" "${4}"
+
+# adjust amcl
+python3 -u ${SCRIPTS_PATH}/amcl_init_node.py
+
+# init rotary encoder
+python3 -u ${SCRIPTS_PATH}/init_encoder.py
